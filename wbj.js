8976509a30ac7b984fd86ef10fa590c1a888c1dc
priyam-2006 <M.Priyamjit@outlook.com>
2024-08-26 22:10:23 +0530

    wbj.js

---
+++ wbj.js
@@ -0,0 +1,15 @@
+function link(){
+    window.open("https://admissions.nic.in/WBJEEB/Applicant/Root/CheckCandIdentity.aspx?enc=yVQCIiq12npg+pcvNJRdc+MheI+oZUchuf9ZqngYKB9TC2Er0Y8MST/kUytnWCwR","_blank");
+  }
+  function view(){
+    window.open("https://wbjeeb.nic.in/wbjee-2/");
+  }
+  function link2(){
+    window.open("https://wb.gov.in/");
+  }
+  function link3(){
+    window.open("https://banglaruchchashiksha.wb.gov.in/");
+  }
+  function link4(){
+    window.open("https://wbjeeb.nic.in/sitemap/");
+  }
